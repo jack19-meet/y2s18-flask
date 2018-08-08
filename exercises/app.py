40e073c57684e61a36c8b68628e99a9a07757df3
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 12:10:20 +0300

    finished without extra

--- exercises/app.py
+++ exercises/app.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    like_same_sport=False
+    arr=["Michael Phelps","Missy Franklin","Mark Spitz","Matt Biondi","Kristin Otta"]
+    if like_same_sport:
+            return render_template("index.html",player_list=arr,like_same_sport=like_same_sport)
+    return render_template("index.html",like_same_sport=like_same_sport)
 if __name__ == '__main__':
    app.run(debug = True)
